c286771d89a81789098b8c5af6532717a7000352
Hairum Qureshi <hqureshi@udel.edu>
2025-07-23 00:25:59 -0400

    added code for configuring Socket IO to run and sent a 'ping' heartbeat to the backend

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,5 +1,40 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./authentication/SignUp";
+import Home from "./Home";
 import "../css/index.css";
+import SignIn from "./authentication/SignIn";
+import useSocketStore from "../stores/useSocketStore";
+import { useEffect } from "react";
+import { useCurrentUser } from "../hooks/useCurrentUser";
+import type { User } from "../interfaces";
 
 export default function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	const { connectSocket, socket } = useSocketStore();
+	const { data: userData } = useCurrentUser();
+
+	useEffect(() => {
+		if ((userData as User)?._id) {
+			connectSocket(userData._id);
+		}
+	}, [userData]);
+
+	useEffect(() => {
+		if (socket) {
+			const interval = setInterval(() => {
+				socket.emit("ping"); // custom ping
+			}, 10000); // every 10 seconds
+			return () => clearInterval(interval);
+		}
+	}, [socket, userData]);
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/sign-up" element={<SignUp />} />
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/" element={<Home />} />
+				{/* <Route path="*" element={<NotFound />} />  */}
+			</Routes>
+		</BrowserRouter>
+	);
 }
