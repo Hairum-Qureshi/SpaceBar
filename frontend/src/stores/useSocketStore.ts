a853028cbdbd36219a1193c99fdb4c99df210f19
Hairum Qureshi <hqureshi@udel.edu>
2025-07-24 14:24:59 -0400

    added code for retrieving the active users from the backend

--- frontend/src/stores/useSocketStore.ts
+++ frontend/src/stores/useSocketStore.ts
@@ -1,33 +1,39 @@
-// stores/useSocketStore.ts
 import { create } from "zustand";
 import { io, Socket } from "socket.io-client";
 
 interface SocketStore {
 	socket: Socket | null;
+	activeUsers: string[];
 	connectSocket: (userId: string) => void;
 	disconnectSocket: () => void;
 }
 
 const useSocketStore = create<SocketStore>(set => ({
 	socket: null,
+	activeUsers: [],
 
 	connectSocket: userID => {
 		const socket = io("http://localhost:3000", {
 			auth: { userID } // if you're sending auth info
 		});
 
-		set({ socket });
+		if (socket.connected) set({ socket });
 
-		// Optionally: handle events globally here
 		socket.on("connect", () => {
 			console.log("Connected:", socket.id);
+
+			setInterval(() => {
+				socket.emit("ping");
+			}, 10000);
+		});
+
+		socket.on("activeUsers", data => {
+			set({ activeUsers: data });
 		});
 
 		socket.on("disconnect", () => {
 			console.log("Disconnected");
 		});
-
-		// Add your custom event handlers here (e.g., messages, heartbeats)
 	},
 
 	disconnectSocket: () => {
