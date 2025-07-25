8c1a5a1700d26d9590ae3c72d66737a1d47bef9e
Hairum Qureshi <hqureshi@udel.edu>
2025-07-25 19:01:59 -0400

    added route for getting all the messages for a conversation based on ID

--- api/routes/conversation.ts
+++ api/routes/conversation.ts
@@ -2,12 +2,18 @@ import express from "express";
 import isAuthenticated from "../middleware/isAuthenticated";
 import {
 	createConversation,
-	getAllConversations
+	getAllChatMessages,
+	getAllConversations,
+	getConversationByID
 } from "../controllers/conversation";
 
 const router = express.Router();
 
 router.post("/create", isAuthenticated, createConversation);
 router.get("/all", isAuthenticated, getAllConversations);
+
+// TODO - add middleware:
+router.get("/:conversationID", isAuthenticated, getConversationByID);
+router.get("/:conversationID/messages", isAuthenticated, getAllChatMessages);
 
 export default router;
