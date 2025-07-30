ff9459b794a8392c916305426d892edc35ebc9d7
Hairum Qureshi <hqureshi@udel.edu>
2025-07-30 19:06:21 -0400

    changed 'conversationID' to 'messagePayload'

--- frontend/src/stores/useSocketStore.ts
+++ frontend/src/stores/useSocketStore.ts
@@ -1,9 +1,18 @@
 import { create } from "zustand";
 import { io, Socket } from "socket.io-client";
 
+interface MessagePayload {
+	conversationID: string;
+	username: string;
+	profilePicture: string;
+	message: string;
+	containedImages: boolean;
+}
+
 interface SocketStore {
 	socket: Socket | null;
 	activeUsers: string[];
+	messagePayload: MessagePayload | undefined;
 	connectSocket: (userId: string) => void;
 	disconnectSocket: () => void;
 }
@@ -11,7 +20,7 @@ interface SocketStore {
 const useSocketStore = create<SocketStore>(set => ({
 	socket: null,
 	activeUsers: [],
-
+	messagePayload: undefined,
 	connectSocket: userID => {
 		const socket = io("http://localhost:3000", {
 			auth: { userID } // if you're sending auth info
@@ -29,6 +38,10 @@ const useSocketStore = create<SocketStore>(set => ({
 
 		socket.on("activeUsers", data => {
 			set({ activeUsers: data });
+		});
+
+		socket.on("newMessage", (messagePayload: MessagePayload) => {
+			set({ messagePayload });
 		});
 
 		socket.on("disconnect", () => {
