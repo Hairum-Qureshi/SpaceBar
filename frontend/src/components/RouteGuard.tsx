659906922f3506ee51d326ecb3e6c6006c594b57
Hairum Qureshi <hqureshi@udel.edu>
2025-08-01 08:09:01 -0400

    added conditional to render Loading component if 'isPending' is true

--- frontend/src/components/RouteGuard.tsx
+++ frontend/src/components/RouteGuard.tsx
@@ -1,6 +1,7 @@
 import { Navigate } from "react-router-dom";
 import { useCurrentUser } from "../hooks/useCurrentUser";
 import type { ReactNode } from "react";
+import Loading from "./Loading";
 
 export default function ProtectedRoute({ children }: { children: ReactNode }) {
 	const { data: userData, isPending } = useCurrentUser();
@@ -8,6 +9,8 @@ export default function ProtectedRoute({ children }: { children: ReactNode }) {
 	if (!userData && !isPending) {
 		return <Navigate to="/sign-up" />;
 	}
+
+	if (isPending) return <Loading />;
 
 	return <>{children}</>;
 }
