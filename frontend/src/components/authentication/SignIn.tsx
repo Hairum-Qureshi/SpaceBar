142eb913282cc48fd67e2e1160cdba4d15f44dea
Hairum Qureshi <hqureshi@udel.edu>
2025-08-01 16:02:55 -0400

    updated UI to be mobile-friendly

--- frontend/src/components/authentication/SignIn.tsx
+++ frontend/src/components/authentication/SignIn.tsx
@@ -11,10 +11,8 @@ export default function SignIn() {
 
 	const { signIn, signInIsPending, formErrors } = useAuth();
 
-	// TODO - add toast notif
-
 	return (
-		<div className="bg-black h-screen w-full flex text-white">
+		<div className="bg-black h-screen lg:w-full lg:flex text-white">
 			<ToastContainer
 				position="top-right"
 				autoClose={2000}
@@ -28,11 +26,11 @@ export default function SignIn() {
 				pauseOnHover={false}
 				transition={Bounce}
 			/>
-			<div className="rounded-2xl w-1/2 h-screen flex items-center justify-center">
+			<div className="lg:rounded-2xl lg:w-2/3 md:w-full h-screen flex items-center justify-center">
 				<img src={spaceImage} alt="Space Image" />
 			</div>
-			<div className="bg-zinc-950 rounded-2xl w-1/2 flex justify-center items-center border-2 border-zinc-600">
-				<div className="w-3/4">
+			<div className="bg-zinc-950 lg:rounded-2xl lg:w-2/3 md:w-full flex justify-center items-center lg:border-2 lg:border-zinc-600">
+				<div className="w-3/4 -mt-5">
 					<div className="mb-8 text-center">
 						<h1 className="text-zinc-400 text-4xl mb-3">
 							Welcome back to <span className="text-sky-500">Space Bar</span>
@@ -65,24 +63,26 @@ export default function SignIn() {
 								<span className="text-gray-500 text-sm">or</span>
 								<hr className="flex-grow border-t border-gray-300" />
 							</div>
-							<div className="flex items-center">
-								<div className="flex flex-col w-full">
-									<label className="text-slate-500 mb-1">Email</label>
-									<input
-										type="email"
-										placeholder="Email"
-										className={`outline-none p-2 text-base bg-slate-900 rounded-md w-full my-2 border ${
-											!formErrors.emailFieldError
-												? "border-sky-600"
-												: "border-red-600"
-										}`}
-										value={email}
-										onChange={e => setEmail(e.target.value)}
-									/>
-								</div>
+							<div className="flex flex-col">
+								<label className="text-slate-500 mb-1">
+									Email<span className="text-red-600 ml-1">*</span>
+								</label>
+								<input
+									type="email"
+									placeholder="Email"
+									className={`outline-none p-2 text-base bg-slate-900 rounded-md w-full my-2 border ${
+										!formErrors.emailFieldError
+											? "border-sky-600"
+											: "border-red-600"
+									}`}
+									value={email}
+									onChange={e => setEmail(e.target.value)}
+								/>
 							</div>
 							<div className="mt-5">
-								<label className="ml-1 text-slate-500">Password</label>
+								<label className="text-slate-500">
+									Password<span className="text-red-600 ml-1">*</span>
+								</label>
 								<input
 									type="password"
 									placeholder="Password"
@@ -97,8 +97,7 @@ export default function SignIn() {
 							</div>
 						</div>
 						<button
-							className="p-2 w-full text-lg rounded-md mt-8 bg-blue-950 hover:cursor-pointer"
-							onSubmit={e => signIn(e, email, password)}
+							className="p-2 mb-10 lg:mb-0 w-full text-lg rounded-md mt-8 bg-blue-950 hover:cursor-pointer"
 							disabled={signInIsPending}
 							type="submit"
 						>
