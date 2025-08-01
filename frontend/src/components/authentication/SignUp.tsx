a5d2a7bb9c6aee9279e06a6a5bea0bfcbe532bff
Hairum Qureshi <hqureshi@udel.edu>
2025-08-01 14:52:03 -0400

    added code for displaying React Toast notifications to show error messages, added logic for rendering colored borders and texts for errors, and added logic for dynamically checking met crieterias for passwords

--- frontend/src/components/authentication/SignUp.tsx
+++ frontend/src/components/authentication/SignUp.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 import { useState } from "react";
 import spaceImage from "../../assets/auth-forms-image.jpg";
+import { Bounce, ToastContainer } from "react-toastify";
 
 export default function SignUp() {
 	const [username, setUsername] = useState("");
@@ -15,18 +16,33 @@ export default function SignUp() {
 		signUpIsPending,
 		validator,
 		passContainsNumsAndSymbols,
-		passwordLengthValid
+		passwordLengthValid,
+		formErrors,
+		passwordsMatch
 	} = useAuth();
 
 	// TODO - add toast notif
 
 	return (
 		<div className="bg-black h-screen w-full flex text-white">
+			<ToastContainer
+				position="top-right"
+				autoClose={2000}
+				hideProgressBar
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				theme="dark"
+				pauseOnHover={false}
+				transition={Bounce}
+			/>
 			<div className="rounded-2xl w-1/2 h-screen flex items-center justify-center">
 				<img src={spaceImage} alt="Space Image" />
 			</div>
 			<div className="bg-zinc-950 rounded-2xl w-1/2 flex justify-center items-center border-2 border-zinc-600">
-				<div className="w-3/4">
+				<div className="w-3/4 -mt-5">
 					<div className="mb-8 text-center">
 						<h1 className="text-zinc-400 text-4xl mb-3">
 							Join <span className="text-sky-500">Space Bar</span>
@@ -68,32 +84,50 @@ export default function SignUp() {
 							</div>
 							<div className="flex items-center">
 								<div className="flex flex-col w-1/2">
-									<label className="mr-1 text-slate-500 mb-1">Username</label>
+									<label className="mr-1 text-slate-500 mb-1">
+										Username<span className="text-red-600 ml-1">*</span>
+									</label>
 									<input
 										type="text"
 										placeholder="Username"
-										className="w-full outline-none p-2 text-base bg-slate-900 rounded-md border border-sky-600 mr-2"
+										className={`outline-none p-2 text-base bg-slate-900 rounded-md w-full my-2 border ${
+											!formErrors.usernameFieldError
+												? "border-sky-600"
+												: "border-red-600"
+										}`}
 										value={username}
 										onChange={e => setUsername(e.target.value)}
 									/>
 								</div>
 								<div className="flex flex-col w-1/2">
-									<label className="ml-2 text-slate-500 mb-1">Email</label>
+									<label className="ml-2 text-slate-500 mb-1">
+										Email<span className="text-red-600 ml-1">*</span>
+									</label>
 									<input
 										type="email"
 										placeholder="Email"
-										className="w-full outline-none p-2 text-base bg-slate-900 rounded-md border border-sky-600 ml-2"
+										className={`outline-none p-2 text-base bg-slate-900 rounded-md w-full ml-2 border ${
+											!formErrors.usernameFieldError
+												? "border-sky-600"
+												: "border-red-600"
+										}`}
 										value={email}
 										onChange={e => setEmail(e.target.value)}
 									/>
 								</div>
 							</div>
 							<div className="mt-5">
-								<label className="ml-1 text-slate-500">Password</label>
+								<label className="ml-1 text-slate-500">
+									Password<span className="text-red-600 ml-1">*</span>
+								</label>
 								<input
 									type="password"
 									placeholder="Password"
-									className="outline-none p-2 text-base bg-slate-900 rounded-md w-full my-2 border border-sky-600"
+									className={`outline-none p-2 text-base bg-slate-900 rounded-md w-full my-2 border ${
+										!formErrors.passwordFieldError
+											? "border-sky-600"
+											: "border-red-600"
+									}`}
 									value={password}
 									onChange={e => setPassword(e.target.value)}
 									onInput={e => validator(e.currentTarget.value)}
@@ -147,14 +181,43 @@ export default function SignUp() {
 								</div>
 							</div>
 							<div>
-								<label className="text-slate-500">Confirm Password</label>
+								<label className="text-slate-500">
+									Confirm Password<span className="text-red-600 ml-1">*</span>
+								</label>
 								<input
 									type="password"
 									placeholder="Confirm Password"
-									className="outline-none p-2 text-base bg-slate-900 rounded-md w-full my-2 border border-sky-600"
+									className={`outline-none p-2 text-base bg-slate-900 rounded-md w-full my-2 border ${
+										!formErrors.confirmPasswordFieldError
+											? "border-sky-600"
+											: "border-red-600"
+									}`}
 									value={confirmPassword}
 									onChange={e => setConfirmPassword(e.target.value)}
+									onInput={e => validator(password, e.currentTarget.value)}
 								/>
+								<div className="flex items-center">
+									<div
+										className={`w-3 h-3 rounded-full border ${
+											!password
+												? "border-slate-600"
+												: !passwordsMatch
+												? "border-red-500 bg-red-600"
+												: "border-green-500 bg-green-600"
+										} my-2`}
+									></div>
+									<p
+										className={`ml-2 ${
+											!password
+												? "text-slate-600"
+												: !passwordsMatch
+												? "text-red-600"
+												: "text-green-600"
+										}`}
+									>
+										Passwords match
+									</p>
+								</div>
 							</div>
 							<p className="text-sm my-2 text-slate-500">
 								By signing up, you agree to Space Bar's&nbsp;
