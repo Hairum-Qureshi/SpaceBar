2321b9cf6fab119c7a3e124298245e6d1d223d1d
Hairum Qureshi <hqureshi@udel.edu>
2025-08-13 07:56:08 -0400

    created upload profile picture route

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -1,9 +1,18 @@
 import express from "express";
-import { getCurrentUser } from "../controllers/user";
+import { getCurrentUser, uploadProfilePicture } from "../controllers/user";
 import isAuthenticated from "../middleware/isAuthenticated";
+import multer from "multer";
+import storage from "../configs/multer-config";
 
 const router = express.Router();
+const upload = multer({ storage });
 
 router.get("/current-user", isAuthenticated, getCurrentUser);
+router.post(
+	"/upload/profile-picture",
+	isAuthenticated,
+	upload.single("profilePicture"),
+	uploadProfilePicture
+);
 
 export default router;
