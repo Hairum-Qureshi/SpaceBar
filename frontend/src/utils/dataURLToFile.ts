306cb42d5fcd894a3dcbf213a5a25d58a699efec
Hairum Qureshi <hqureshi@udel.edu>
2025-08-17 11:45:42 -0400

    added optional 'fileFor' param to check whether it's for a group chat; if it is, name it something else

--- frontend/src/utils/dataURLToFile.ts
+++ frontend/src/utils/dataURLToFile.ts
@@ -1,11 +1,22 @@
 import axios from "axios";
 
-export async function dataURLToFile(dataURLToFile: string): Promise<File> {
+export async function dataURLToFile(
+	dataURLToFile: string,
+	fileFor?: string
+): Promise<File> {
 	const response = await axios.get(dataURLToFile, { responseType: "blob" });
 	const blob = response.data;
 	const originalFile = await fetch(dataURLToFile).then(res => res.blob());
 
-	return new File([blob], `message-${Date.now()}`, {
-		type: originalFile.type
-	});
+	return new File(
+		[blob],
+		`${
+			fileFor && fileFor === "groupChat"
+				? `gc-${Date.now()}`
+				: `message-${Date.now()}`
+		}`,
+		{
+			type: originalFile.type
+		}
+	);
 }
