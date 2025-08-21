724668cd5425654eecedb4b5e5afc30583392269
Hairum Qureshi <hqureshi@udel.edu>
2025-08-21 09:04:14 -0400

    added conditional logic to check what the image is for and added null checks before accessing the properties instead of immediately destructuring properties

--- api/utils/file-related/getFileUploadData.ts
+++ api/utils/file-related/getFileUploadData.ts
@@ -1,13 +1,15 @@
 import { Request } from "express";
-import { FileUploadData, IUser } from "../../interfaces";
+import { FileUploadData, IConversation, IUser } from "../../interfaces";
 import path from "path";
 import fs from "fs";
 import User from "../../models/User";
+import Conversation from "../../models/inbox/Conversation";
 
 export async function getFileUploadData(
 	req: Request,
 	currUID: string,
 	imageFor: string,
+	conversationID?: string
 ): Promise<FileUploadData> {
 	const ext: string = req.file?.mimetype.split("/").pop()!;
 	const FOLDER_PATH = path.join(__dirname, "../../uploads");
@@ -15,7 +17,17 @@ export async function getFileUploadData(
 		imageFor === "pfp" ? `${currUID}.${ext}` : `gc-${currUID}.${ext}`
 	}`;
 	const fileBuffer: Buffer = fs.readFileSync(`${FOLDER_PATH}/${fileName}`);
-	const { pfpImageID }: IUser = (await User.findById(currUID)) as IUser;
 
-	return { FOLDER_PATH, fileName, fileBuffer, pfpImageID };
+	let imageID = "";
+	if (imageFor === "pfp") {
+		const user: IUser = (await User.findById(currUID)) as IUser;
+		imageID = user?.pfpImageID;
+	} else {
+		const conversation: IConversation = (await Conversation.findById(
+			conversationID
+		)) as IConversation;
+		imageID = conversation?.groupChatPhotoImageID;
+	}
+
+	return { FOLDER_PATH, fileName, fileBuffer, imageID };
 }
