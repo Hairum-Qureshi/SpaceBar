b01b9cef0b148122d93169be90a4dd083d0abe5f
Hairum Qureshi <hqureshi@udel.edu>
2025-08-21 07:20:12 -0400

    moved helper functions to utils folder and imported them

--- api/controllers/user.ts
+++ api/controllers/user.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
-import { IUser } from "../interfaces";
+import { IConversation, IUser } from "../interfaces";
 import User from "../models/User";
 import path from "path";
 import fs from "fs";
 import imagekit from "../configs/imagekit-config";
+import { uploadImageToImageKit } from "../utils/file-related/uploadImageToImageKit";
+import { getFileUploadData } from "../utils/file-related/getFileUploadData";
 
 const getCurrentUser = async (req: Request, res: Response): Promise<void> => {
 	try {
@@ -22,57 +24,15 @@ const getCurrentUser = async (req: Request, res: Response): Promise<void> => {
 	}
 };
 
-function uploadImageToImageKit(
-	fileBuffer: Buffer,
-	fileName: string,
-	currUID: string,
-	res: Response,
-	FOLDER_PATH: string
-) {
-	imagekit
-		.upload({
-			file: fileBuffer,
-			fileName: `pfp-${fileName}`
-		})
-		.then(async response => {
-			const updatedUser: IUser = (await User.findByIdAndUpdate(
-				currUID,
-				{
-					profilePicture: response.url,
-					pfpImageID: response.fileId
-				},
-				{
-					new: true
-				}
-			)) as IUser;
-
-			fs.unlink(path.join(FOLDER_PATH, fileName), err => {
-				if (err) throw err;
-			});
-
-			res.status(200).json(updatedUser);
-		})
-		.catch(error => {
-			console.error("Image upload error:", error);
-
-			fs.unlink(path.join(FOLDER_PATH, fileName), err => {
-				if (err) throw err;
-			});
-		});
-}
-
 const uploadProfilePicture = async (
 	req: Request,
 	res: Response
 ): Promise<void> => {
 	try {
-		const ext: string = req.file?.mimetype.split("/").pop()!;
-		const FOLDER_PATH = path.join(__dirname, "..", "./uploads");
 		const currUID: string = req.user._id;
-		const fileName = `${currUID}.${ext}`;
-		const fileBuffer: Buffer = fs.readFileSync(`${FOLDER_PATH}/${fileName}`);
 
-		const { pfpImageID }: IUser = (await User.findById(currUID)) as IUser;
+		const { FOLDER_PATH, fileName, fileBuffer, pfpImageID } =
+			await getFileUploadData(req, currUID);
 
 		imagekit.getFileMetadata(pfpImageID, (error, result) => {
 			if (error) {
@@ -84,7 +44,14 @@ const uploadProfilePicture = async (
 					});
 				}
 			}
-			uploadImageToImageKit(fileBuffer, fileName, currUID, res, FOLDER_PATH);
+			uploadImageToImageKit(
+				fileBuffer,
+				fileName,
+				currUID,
+				res,
+				FOLDER_PATH,
+				"pfp"
+			);
 		});
 	} catch (error) {
 		console.error(
