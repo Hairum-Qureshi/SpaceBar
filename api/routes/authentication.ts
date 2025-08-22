d640fef739d00a528b845a3c0ee9e4f2042ee7db
Hairum Qureshi <hqureshi@udel.edu>
2025-08-22 11:08:33 -0400

    added Google sign in POST route

--- api/routes/authentication.ts
+++ api/routes/authentication.ts
@@ -1,10 +1,17 @@
 import express from "express";
-import { signIn, signOut, signUp } from "../controllers/authentication";
+import {
+	signIn,
+	signInWithGoogle,
+	signOut,
+	signUp
+} from "../controllers/authentication";
+import isAuthenticated from "../middleware/isAuthenticated";
 
 const router = express.Router();
 
 router.post("/sign-up", signUp);
 router.post("/sign-in", signIn);
-router.post("/sign-out", signOut);
+router.post("/google/sign-in", signInWithGoogle);
+router.post("/sign-out", isAuthenticated, signOut);
 
 export default router;
