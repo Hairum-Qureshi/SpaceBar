a4731534119449169a35233747f4a33bb2d4232d
Hairum Qureshi <hqureshi@udel.edu>
2025-08-22 19:33:02 -0400

    fixed JSON import for service account JSON file and typecasted import variable

--- api/configs/firebase/firebase-config.ts
+++ api/configs/firebase/firebase-config.ts
@@ -1,6 +1,8 @@
-import admin from "firebase-admin";
-const serviceAccount = "../firebase/serviceAccount.json";
+import admin, { ServiceAccount } from "firebase-admin";
+import serviceAccount from "./serviceAccount.json";
 
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount)
+	credential: admin.credential.cert(serviceAccount as ServiceAccount)
 });
+
+export default admin;
