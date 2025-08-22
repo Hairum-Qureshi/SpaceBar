2bf134c75fffe590a987c409f1cb274eeffe3d67
Hairum Qureshi <hqureshi@udel.edu>
2025-08-22 10:44:39 -0400

    added alert 'coming soon!' message when the user presses the 'sign up with Apple' button

--- frontend/src/components/authentication/SignUp.tsx
+++ frontend/src/components/authentication/SignUp.tsx
@@ -70,7 +70,13 @@ export default function SignUp() {
 									</span>
 									&nbsp; Google
 								</button>
-								<button className="p-2 border flex items-center justify-center border-sky-600 rounded-md w-1/2 ml-1 hover:cursor-pointer">
+								<button
+									className="p-2 border flex items-center justify-center border-sky-600 rounded-md w-1/2 ml-1 hover:cursor-pointer"
+									onClick={e => {
+										e.preventDefault();
+										alert("Coming soon!");
+									}}
+								>
 									<span className="text-xl">
 										<FaApple />
 									</span>
