bc5b9001715a7c26c4882d2b62923d22aee15df6
Hairum Qureshi <hqureshi@udel.edu>
2025-08-23 00:29:42 -0400

    re-arranged imports

--- api/routes/message.ts
+++ api/routes/message.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import isAuthenticated from "../middleware/isAuthenticated";
 import { sendMessage } from "../controllers/message";
+import multer from "multer";
+import storage from "../multer-config";
 
 const router = express.Router();
 
-import multer from "multer";
-import storage from "../multer-config";
 const upload = multer({ storage });
 
 router.post("/send", isAuthenticated, upload.array("images", 4), sendMessage);
