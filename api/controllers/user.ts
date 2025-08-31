8d25231af0e286a8218ef02820b3892c42fbb801
Hairum Qureshi <hqureshi@udel.edu>
2025-08-31 19:55:18 -0400

    added soft delete logic which replaces identifiable information

--- api/controllers/user.ts
+++ api/controllers/user.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from "express";
-import { IConversation, IUser } from "../interfaces";
+import { IUser } from "../interfaces";
 import User from "../models/User";
-import path from "path";
-import fs from "fs";
 import imagekit from "../configs/imagekit-config";
 import { uploadImageToImageKit } from "../utils/file-related/uploadImageToImageKit";
 import { getFileUploadData } from "../utils/file-related/getFileUploadData";
+import { v4 as uuidv4 } from "uuid";
 
 const getCurrentUser = async (req: Request, res: Response): Promise<void> => {
 	try {
@@ -31,8 +30,12 @@ const uploadProfilePicture = async (
 	try {
 		const currUID: string = req.user._id;
 
-		const { FOLDER_PATH, fileName, fileBuffer, imageID:pfpImageID } =
-			await getFileUploadData(req, currUID, "pfp");
+		const {
+			FOLDER_PATH,
+			fileName,
+			fileBuffer,
+			imageID: pfpImageID
+		} = await getFileUploadData(req, currUID, "pfp");
 
 		imagekit.getFileMetadata(pfpImageID, (error, result) => {
 			if (error) {
@@ -63,4 +66,52 @@ const uploadProfilePicture = async (
 	}
 };
 
-export { getCurrentUser, uploadProfilePicture };
+const deleteUserAccount = async (
+	req: Request,
+	res: Response
+): Promise<void> => {
+	try {
+		// TODO - update the 'delete convo' logic where if the user is deleted and the other user wants to delete the chat, it'll delete the convo entirely including all the messages
+
+		// TODO - for signing in, make sure you add a check to see if the username doesn't include 'DELETED_USER_' in it to prevent users from signing in on deleted accounts
+
+		const currUID: string = req.user._id;
+
+		const user: IUser = (await User.findByIdAndUpdate(
+			currUID,
+			{
+				$set: {
+					username: `DELETED_USER_${uuidv4().substring(0, 8)}`,
+					email: "DELETED_USER",
+					password: "DELETED_USER",
+					profilePicture:
+						"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx-NP_Wn_xnnzlQYXWRJorxpkeyQtkKf957g&s",
+					isDeleted: true,
+					deletedAt: new Date()
+				}
+			},
+			{ new: true }
+		)) as IUser;
+
+		// TODO - may need to check if it was actually deleted from Image Kit
+		imagekit.deleteFile(user.pfpImageID, error => {
+			if (error) console.log(error);
+		});
+
+		const updatedUser: IUser = (await User.findByIdAndUpdate(currUID, {
+			$set: {
+				pfpImageID: ""
+			}
+		})) as IUser;
+
+		res.status(200).json({ updatedUser });
+	} catch (error) {
+		console.error(
+			"Error in user.ts file, deleteUserAccount function controller".red.bold,
+			error
+		);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
+
+export { getCurrentUser, uploadProfilePicture, deleteUserAccount };
